e425d39c29f2da9990a0a12bdc6376bc47d4f9bf
Assasin Protocol Eth <123295385+Assassinprotocol@users.noreply.github.com>
2026-03-14 07:59:21 +0700

    Update dashboard_config.py

--- dashboard/dashboard_config.py
+++ dashboard/dashboard_config.py
@@ -11,7 +11,7 @@ DASHBOARD_PORT = 5000
 # ================================
 # SECURITY
 # ================================
-JWT_SECRET             = "KMZWAY87AA"
+JWT_SECRET             = ""
 SESSION_TIMEOUT_MINUTES = 60
 REMEMBER_ME_DAYS       = 30
 RATE_LIMIT_PER_MINUTE  = 60
@@ -28,29 +28,29 @@ DB_PATH = r"C:\AI\hansen_engine\dashboard\users.db"
 # ================================
 # ADMIN
 # ================================
-ADMIN_USERNAME = "Hansen"
-ADMIN_PASSWORD = "Excuted90"
+ADMIN_USERNAME = ""
+ADMIN_PASSWORD = ""
 
 # ================================
 # EMAIL (Gmail SMTP)
 # ================================
 SMTP_HOST     = "smtp.gmail.com"
 SMTP_PORT     = 587
-SMTP_EMAIL    = "dcaprioleonardo64@gmail.com"
-SMTP_PASSWORD = "nhchhhcnbmqkotyy"
+SMTP_EMAIL    = "UR EMAIL"
+SMTP_PASSWORD = ""
 
 # ================================
 # PAYMENT WALLETS
 # ================================
 WALLETS = {
-    "BTC":        "bc1ptknrx96kuyudg5xnqc3wq6y4hdckqxay5cgq808um7e7gnygwc8swnv8xn",
-    "USDT_BSC":   "0xC0D018E7278De2611B67c7e59b9b45570dC59aEB",
-    "USDT_ARB":   "0xC0D018E7278De2611B67c7e59b9b45570dC59aEB",
-    "USDT_BASE":  "0xC0D018E7278De2611B67c7e59b9b45570dC59aEB",
-    "USDT_HYPER": "0xC0D018E7278De2611B67c7e59b9b45570dC59aEB",
-    "USDT_SOL":   "DvbvPU25fftYV6LA21FxQ7B5y4bb6C6rQxMCLR5ariF",
-    "USDT_APT":   "0x88001f20fc5d01220851e8cb9353e3cddc5fadc29c5cf41ec5d1caece0db6d73",
-    "USDC_APT":   "0x88001f20fc5d01220851e8cb9353e3cddc5fadc29c5cf41ec5d1caece0db6d73",
+    "BTC":        "UR WALLET",
+    "USDT_BSC":   "",
+    "USDT_ARB":   "",
+    "USDT_BASE":  "",
+    "USDT_HYPER": "",
+    "USDT_SOL":   "",
+    "USDT_APT":   "",
+    "USDC_APT":   "",
 }
 
 # ================================
@@ -75,9 +75,9 @@ PLANS = {
 
 PAYMENT_MIN_USDT = 4.50
 PAYMENT_MIN_BTC  = 0.000048
-BSCSCAN_API_KEY   = "JU2JMA7W7GN1IMN624N7141FE7TY3YE8SH"
-ARBISCAN_API_KEY  = "JU2JMA7W7GN1IMN624N7141FE7TY3YE8SH"
-ETHERSCAN_API_KEY = "JU2JMA7W7GN1IMN624N7141FE7TY3YE8SH"
+BSCSCAN_API_KEY   = ""
+ARBISCAN_API_KEY  = ""
+ETHERSCAN_API_KEY = ""
 # ================================
 # TRIAL
 # ================================
